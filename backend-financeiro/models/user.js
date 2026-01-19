a7cbef6298628beda0d7a16cc88a3766ef310cbf
Rayanne Karyne <karynesantos0901@gmail.com>
2026-01-18 21:09:43 -0300

    Criação da tabela usuários

---
+++ backend-financeiro/models/user.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id_user: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+
+});
+
+module.exports = User;
