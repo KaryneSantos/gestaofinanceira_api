2f80b04d3b4bd1ee21d6b14ec00349bf1779d5e0
Rayanne Karyne <karynesantos0901@gmail.com>
2026-01-19 23:41:57 -0300

    CRUD RENDA

--- backend-financeiro/routes/renda.js
+++ backend-financeiro/routes/renda.js
@@ -3,72 +3,114 @@ const router = express.Router();
 
 const Renda = require('../models/renda');
 
-// cadastrar renda
+// Cadastrar renda
 router.post('/', async (req, res) => {
      try {
-            const { descricao, valor } = req.body;
 
-            if(!descricao || !valor) {
-                return res.status(400).json({ error: "Preenchar todos os campos" })
+         if (!req.session.user) {
+            return res.status(401).json({ error: 'Usuário não autenticado' });
+        }
+        
+           const {valor, descricao, data, categoria} = req.body;
+           const errors = [];
+            const usuario_id = req.session.user.id;
+
+            if(!valor || !descricao || !data || !categoria) {
+            errors.push('Todos os campos são obrigatórios.');
+            return res.status(400).json({errors});
             }
 
-            await Renda.create(descricao, valor);
+             if(isNaN(parseFloat(valor)) || parseFloat(valor) <= 0) {
+            errors.push('Valor inválido');
+            return res.status(400).json({errors});
+            }
 
-            return res.json({ mensagem: "Renda cadastrada com sucesso!" })
+            const rendaExistente = await Renda.findOne({
+            where: {descricao, data, usuario_id}
+            });
+
+            if(rendaExistente) {
+            errors.push('Renda já cadastrada neste dia.');
+            return res.status(400).json({errors});
+            }
+
+             const novaRenda = await Renda.create({valor, descricao, data, categoria, usuario_id});
+             res.status(201).json(novaRenda);
+
         } catch (error) {
             console.error(error)
             return res.status(500).json({ error: "Error ao cadastrar renda" })
         }
 });
 
-// listar renda
+// Listar todas as rendas
 router.get('/', async (req, res) => {
     try {
-        const rendas = await Renda.findAll();
-        res.json(rendas);
+        const usuario_id = req.session.user.id;
+
+        const renda = await Renda.findAll({
+            where: { usuario_id },
+            order: [['data', 'DESC']]
+        });
+
+        res.json(renda);
     } catch (error) {
         res.status(500).json({ error: "Error ao buscar rendas" });
     }
 });
 
-// deletar renda
-router.delete('/:id', async (req, res) => {
-    try{
-        const id = req.params.id;
-        const renda = await Renda.findByPk(id);
-
-        if(!renda) {
-            return res.status(404).json({ error: "Renda não encontrada" });
-        }
-
-        await renda.destroy();
-
-        return res.json({ mensagem: "Renda deletada com sucesso!" })
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ Error: "Error ao deletar renda" });
-    }
-});
-
-// atualizar renda
+// Atualizar renda
 router.put('/:id', async (req, res) => {
     try{
-        const id = req.params.id;
-        const { descricao, valor } = req.body;
 
-        const renda = await Renda.findByPk(id)
+        const {id} = req.params;
+        const {valor, descricao, data, categoria} = req.body;
+        const usuario_id = req.session.user.id;
+
+        const renda = await Renda.findOne({
+            where: {id_renda: id, usuario_id}
+        });
 
         if(!renda) {
-            return res.status(404).json({ error: "Renda não encontrada" })
+            return res.status(404).json({error: 'Renda não encontrada.'});
         }
 
-        await renda.update({ descricao, valor });
-        res.json({ mensagem: "Renda atualizada com sucesso!", renda })
+        await renda.update({
+            valor,
+            descricao,
+            data,
+            categoria
+        });
+
+        res.json({message: 'Renda atualizada com sucesso.', renda});
 
 
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "Error ao atualizar renda" });
+    }
+});
+
+// Deletar renda
+router.delete('/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const usuario_id = req.session.user.id;
+
+        const renda = await Renda.findOne({
+            where: {id_renda: id, usuario_id}            
+        });
+
+        if (!renda) {
+            return res.status(404).json({ error: 'Renda não encontrada.' });
+        }
+
+        await renda.destroy();
+
+        res.json({ message: 'Renda removida com sucesso.' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ Error: "Error ao deletar renda" });
     }
 });
 
